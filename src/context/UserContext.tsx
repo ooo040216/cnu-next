8c2c6dcf75973a871ff2aed9494e46bc51c9e5ab
202302597이예원 <yewonlee@o.cnu.ac.kr>
2025-06-17 22:46:44 +0900

    Update UserContext.tsx

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,35 +1,35 @@
+@@ -1,13 +1,16 @@
 "use client";
 import { createContext, ReactNode, useContext, useState } from "react";
-
 // 과제 1.1 UserContext 구현
-
 // User
 interface User {
   name: string;
   // age: number
   // 추가하고 싶은 속성들 ...
+  userId: string;
+  age: number;
+  phoneNumber: string;
 }
 // UserContextType
 interface UserContextType {
-  user: User;
-  setUser: (user: User) => void;
-}
-
-//  1. createContext
-export const UserContext = createContext<UserContextType | undefined>(
-  undefined
-);
+@@ -22,7 +25,12 @@ export const UserContext = createContext<UserContextType | undefined>(
 
 // 2. Provider 생성
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User>({ name: "" });
+  const [user, setUser] = useState<User>({
+    name: "이예원", 
+    userId: "yewonlee",
+    age: 22,
+    phoneNumber: "010-1234=5678"
+  });
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
 };
-
 // 3. user 정보를 사용하기 위한 custom hook
 export const useUser = () => {
   const context = useContext(UserContext);
